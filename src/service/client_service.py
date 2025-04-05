a0ab80e374c738a91bbdedee0fe78c19b6c690a5
wiseCirno <78086755+wiseCirno@users.noreply.github.com>
2025-04-05 17:42:35 +0800

    :sparkles: Add param for base_url

---
+++ src/service/client_service.py
@@ -0,0 +1,23 @@
+from typing import Dict, Optional
+
+from httpx import AsyncClient
+
+from src.config import PROXY
+from .proxy_service import ProxyService
+
+
+def new_async_client(
+        headers: Optional[Dict] = None,
+        base_url: Optional[str] = None
+) -> AsyncClient:
+    if PROXY:
+        return AsyncClient(
+            headers = headers,
+            proxy = ProxyService.new_proxy(PROXY),
+            base_url = base_url
+        )
+
+    return AsyncClient(
+        headers = headers,
+        base_url = base_url
+    )
