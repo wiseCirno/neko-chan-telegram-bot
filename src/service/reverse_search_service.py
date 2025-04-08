426b8e56a6d7e23fe338b8aaaccf26278b495f50
wiseCirno <78086755+wiseCirno@users.noreply.github.com>
2025-04-09 00:56:27 +0800

    :sparkles: Add new reverse search (not implemented)

---
+++ src/service/reverse_search_service.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from typing import Optional, Union
+
+
+class ReverseSearchService:
+    @staticmethod
+    async def anime_trace_search(
+            url: Optional[str] = None,
+            file: Union[str, bytes, Path, None] = None
+
+    ):
+        pass
